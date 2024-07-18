5049519c905085e0c11105f0ecd057fb7cb184c2
Kristian Zarębski <krizar312@gmail.com>
2024-07-18 18:50:27 +0100

    use variable for minor version in uninstall

--- tools/chocolateyuninstall.ps1
+++ tools/chocolateyuninstall.ps1
@@ -3,11 +3,11 @@ $versionParts = $packageVersion -split '\.'
 $majorMinorVersion = "$($versionParts[0]).$($versionParts[1])"
 $installPath = "${env:ProgramFiles(x86)}\Stunt Rally $majorMinorVersion"
 $registryPath = "HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Stunt Rally"
-$shortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Stunt Rally 3.1\Stunt Rally 3.lnk"
+$shortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Stunt Rally $majorMinorVersion\Stunt Rally 3.lnk"
 
 # No silent option to NSIS installer so have to manually uninstall
 if(Test-Path $shortcutPath) {
-    Remove-Item "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Stunt Rally 3.1\Stunt Rally 3.lnk"
+    Remove-Item "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Stunt Rally $majorMinorVersion\Stunt Rally 3.lnk"
 }
 
 if(Test-Path $installPath) {
