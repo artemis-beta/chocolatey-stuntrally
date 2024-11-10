a046eb77e7e4b5bf4bb67bdebf80d270933268e2
Kristian Zarębski <krizar312@gmail.com>
2024-11-10 12:30:59 +0000

    Release for v3.2

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'Stop';
-$checksum = "ed0df9a8d7df3fc8ef2ba953c29afd68405ee55e2bfd3c4eefbedeab2b91e7d0b4c26b3991b3ab58fa44300b6de9dc326e0b3b1c4d665aa088cf78522bde10cc"
+$checksum = "abff07d8cc1880e70b4eb44cfff25f6cecd03d8aca544f5ff142320342616f525185fa1617b5c0290a604a1e9670bfac422b5b4435a06b64b620516dea08f909"
 $checksum_icon = "92ab5274c605d291711bfc69e9d0214f5fa99d6c3acf6303b07326bc059211971e916cee750a547869943a91ca82d5142e4f31065cc0e6e76dea5eb08730a4be"
 $repository = "stuntrally/stuntrally3"
 $versionParts = $packageVersion -split '\.'
@@ -18,7 +18,7 @@ $installArgs = @{
     checksumType  = 'sha512'
 }
 $shortcutArgs = @{
-    ShortcutFilePath    = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Stunt Rally 3.1\Stunt Rally.lnk"
+    ShortcutFilePath    = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Stunt Rally 3.2\Stunt Rally.lnk"
     TargetPath          = "$exePath"
     IconLocation        = "$installDirectory\StuntRallyIcon.ico"
     WorkingDirectory    = "$installDirectory"
@@ -30,5 +30,5 @@ Install-ChocolateyPackage @installArgs
 # Manually Create Link so we can set Icon
 Get-WebFile -Url "https://raw.githubusercontent.com/artemis-beta/chocolatey-stuntrally/main/media/StuntRallyIcon.ico" -FileName $installDirectory\StuntRallyIcon.ico
 Get-ChecksumValid -File $installDirectory\StuntRallyIcon.ico -Checksum $checksum_icon -ChecksumType 'sha512'
-Remove-Item "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Stunt Rally 3.1\Stunt Rally 3.lnk"
+Remove-Item "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Stunt Rally 3.2\Stunt Rally 3.lnk"
 Install-ChocolateyShortcut @shortcutArgs
